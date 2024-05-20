c38d844dd3d9c06be7a5d66e87d62a1d662b0075
Raj00377 <dev.rajkumar.codes@gmail.com>
2024-05-20 23:16:28 +0530

    ✨ Detect Scroll in parent element overflows

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,23 +1,61 @@
-import { useEffect, useState, useCallback, } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 
-const useScroll = () => {
+type ParentElementType = HTMLElement | Window | null;
+
+const getScrollParent = (node: HTMLElement | null): ParentElementType => {
+  const isElement = node instanceof HTMLElement;
+  const overflowY = isElement && window.getComputedStyle(node).overflowY;
+  const isScrollable = overflowY !== 'visible' && overflowY !== 'hidden';
+
+  if (!node) {
+    return null;
+  } else if (isScrollable && node.scrollHeight >= node.clientHeight) {
+    return node;
+  }
+
+  return getScrollParent(node.parentElement) || window;
+}
+
+const getParentProps = (parentElement: ParentElementType, ref: React.MutableRefObject<HTMLDivElement | null>) => {
+  return parentElement instanceof Window ?
+    {
+      parentHeight: parentElement?.innerHeight || 0,
+      elementTop: ref.current?.offsetTop || 0
+    } :
+    {
+      parentHeight: parentElement?.clientHeight || 0,
+      elementTop: (ref?.current?.offsetTop || 0) - (parentElement?.offsetTop || 0)
+    };
+}
+
+const useScroll = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
 
   const [scrollY, setScrollY] = useState(0);
 
-  const onScroll = useCallback(() => {
-    setScrollY(window.scrollY);
-  }, []);
+  const [parentElement, setParentElement] = useState<ParentElementType>(null);
+
+  const { parentHeight, elementTop } = useMemo(() => getParentProps(parentElement, ref), [parentElement])
+
+  useEffect(() => {
+    setParentElement(getScrollParent(ref.current))
+  }, [ref?.current])
+
+  const onScroll = () => {
+    setScrollY(() => {
+      return parentElement instanceof Window ? parentElement.scrollY : (parentElement?.scrollTop || 0);
+    });
+  };
 
   useEffect(() => {
     //add eventlistener to window
-    window.addEventListener("scroll", onScroll, { passive: true });
+    parentElement?.addEventListener("scroll", onScroll, { passive: true });
     // remove event on unmount to prevent a memory leak with the cleanup
     return () => {
-      window.removeEventListener("scroll", onScroll);
+      parentElement?.removeEventListener("scroll", onScroll);
     }
-  }, []);
+  }, [ref?.current, parentElement]);
 
-  return scrollY;
+  return { scrollY, parentElement, parentHeight, elementTop };
 
 }
 
