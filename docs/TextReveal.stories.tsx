bf899a1a608921c3a0d02b865e5020b2f4b27d94
Raj00377 <dev.rajkumar.codes@gmail.com>
2024-05-23 12:55:42 +0530

    📝 Stories to support linear gradient

--- docs/TextReveal.stories.tsx
+++ docs/TextReveal.stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import TextReveal from '../src/TextReveal';
 import './text-reveal.stories.css';
@@ -18,10 +19,11 @@ export default meta;
 type Story = StoryObj<typeof TextReveal>;
 
 
-export const Example: Story = {
+export const example_solid_colors: Story = {
   parameters: {
     layout: 'fullscreen',
   },
+  name : "Example: Solid colors",
   decorators: [
     (Story) => (
       <div style={{ height: '200vh', fontFamily: 'sans-serif', padding: '1rem', background: 'black' }}>
@@ -33,11 +35,50 @@ export const Example: Story = {
       </div>
     )],
   args: {
-    text: ['Hello world'],
+    text: ['Hello world', 'Hail JS 🙌'],
     textColor: '#3d3d3d',
     fillColor: '#f47979',
     fillSpeed: 100,
     fillDelay: 3.5,
-    fillDirection: "top-bottom"
+    fillDirection: "left-right"
   },
+};
+
+export const example_gradient_color: Story = {
+  parameters: {
+    layout: 'fullscreen',
+  },
+  name : "Example: Linear Gradients",
+  decorators: [
+    (Story) => (
+      <div style={{ height: '200vh', fontFamily: 'sans-serif', padding: '1rem', background: 'black' }}>
+        <div style={{ color: 'white', marginBottom: '0.5rem' }}>👆 Reload 🔄 once you did any change in <u>Controls</u></div>
+        <div style={{ fontSize: '1.5rem', color: 'white' }}>Scroll down to see the text reveal</div>
+        <div style={{ fontSize: '5rem', marginTop: '60vh' }}>
+          <Story />
+        </div>
+      </div>
+    )],
+  args: {
+    text: ['Hello world', 'Hail JS 🙌'],
+    textColor: '#3d3d3d',
+    fillColor: 'linear-gradient(90deg, #12C2E9 0%, #c471ed 50%, #f64f59 100%)',
+    fillSpeed: 100,
+    fillDelay: 3.5,
+    fillDirection: "left-right"
+  },
+
+  argTypes: {
+    textColor: {
+      control: {
+        type: 'text',
+      },
+      description : 'sadfsafsd dsf sdf sad'
+    },
+    fillColor : {
+      control: {
+        type: 'text',
+      },
+    }
+  }
 };
