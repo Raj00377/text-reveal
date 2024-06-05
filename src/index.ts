8073d4288cdddb401726cca784f091158fff3005
Raj00377 <dev.rajkumar.codes@gmail.com>
2024-06-05 13:09:25 +0530

    🚚 Import from single source of truth

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import TextReveal from "./TextReveal";
+import useScroll from "./hooks/useScroll";
+import useCounter from "./hooks/useCounter";
+import generateClipPath from "./utils/generateClipPath";
+import getColor from "./utils/getColor";
 
 export default TextReveal;
+
+export {
+  useScroll,
+  useCounter,
+  generateClipPath,
+  getColor
+}
